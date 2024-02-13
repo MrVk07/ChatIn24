c433accb80987769c0f6bbfbb856bf7a07575d71
Vineet Karwa <vineetkarwa123@gmail.com>
2024-02-13 22:37:50 +0530

    Add: Client Initial Setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ChatApp from './components/ChatApp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ChatApp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ChatApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatApp = () => {
+    return (
+        <div className='text-3xl m-2 p-2'>ChatApp</div>
+    )
+}
+
+export default ChatApp
